fb3252303717965e50ca45cf2b9193af58db6b71
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-04 03:45:31 +0200

    feat(blog): implement basic blog views and templates - Add URL routes and views for home, all blogs, and blog detail; create corresponding empty templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from django.urls import URLPattern
+from django.urls import URLPattern, path
 
 from . import views
 
 urlpatterns : list[URLPattern] = [
-
+    path('' , views.starting_page , name='blog_home') ,
+    path('blogs/' , views.blogs , name="all_blogs") ,
+    path('blogs/<slug:slug>' , views.blog_detail , name='blog_detail')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -17,21 +17,21 @@ BLOGS: List[Blog] = [
         "title": "First Steps with Django",
         "image": "blog/images/django-first-steps.jpg",
         "excerpt": "Getting started with projects, apps, and runserver.",
-        "content": "Full post content goes here...",
+        "content": "Full blog content goes here...",
     },
     {
         "slug": "template-inheritance-like-a-pro",
         "title": "Template Inheritance Like a Pro",
         "image": "blog/images/templates.jpg",
         "excerpt": "Build maintainable UIs with extends, blocks, and includes.",
-        "content": "Full post content goes here...",
+        "content": "Full blog content goes here...",
     },
     {
         "slug": "static-files-that-scale",
         "title": "Static Files that Scale",
         "image": "blog/images/static-files.jpg",
         "excerpt": "Global vs app static, STATICFILES_DIRS, and best practices.",
-        "content": "Full post content goes here...",
+        "content": "Full blog content goes here...",
     },
 ]
 def get_latest_blogs(n:int = 3) ->List[Blog] :
@@ -46,10 +46,10 @@ def find_blog_by_slug(slug:str)->Blog|None:
 def starting_page(request:HttpRequest)->HttpResponse :
     context:Dict[str, List[Blog]] = {'blogs' : get_latest_blogs()}
     return render(request , 'blog/index.html' , context)
-def posts(request:HttpRequest)->HttpResponse :
+def blogs(request:HttpRequest)->HttpResponse :
     context:Dict[str, List[Blog]] = {'blog' : BLOGS}
     return render(request, 'blog/all-blogs.html' , context)
-def post_detail(request:HttpRequest , slug:str)->HttpResponse :
+def blog_detail(request:HttpRequest , slug:str)->HttpResponse :
     blog:Blog|None = find_blog_by_slug(slug)
     if blog is None :
         raise Http404('Page not found')
