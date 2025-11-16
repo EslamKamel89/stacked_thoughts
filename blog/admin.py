10bfa188188be32a92b2ea21a4220dac46a56630
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-16 04:52:50 +0200

    feat(blog): register models in admin and simplify Post-Tag relationship

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Post, Tag
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin): # type: ignore
+    list_display = ('first_name' , 'last_name'  , 'email')
+    search_fields = ('first_name' , 'last_name' , 'email')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin): # type: ignore
+    list_display=('caption',)
+    search_fields=('caption',)
+
+
+@admin.register(Post)
+class  PostAdmin(admin.ModelAdmin): # type: ignore
+    list_display =('title' , 'excerpt' , 'image' , 'content' , 'slug' , 'created_at')
+    search_fields =('title' , 'content' , 'excerpt')
+    list_filter =('tags' , 'author')
+    prepopulated_fields = {'slug':('title',)}
+    filter_horizontal = ('tags',)
+
--- blog/models.py
+++ blog/models.py
@@ -39,7 +39,12 @@ class Post (BaseModel) :
     ])
     slug = models.SlugField(unique=True , db_index=True)
     author = models.ForeignKey(Author , on_delete=models.SET_NULL , null=True, related_name='posts')
-    tags = models.ManyToManyField("Tag" , through='PostTag' , related_name='posts' , blank=True) # type: ignore
+    tags = models.ManyToManyField(# type: ignore
+        "Tag" ,
+        related_name='posts' ,
+        blank=True ,
+        # through='PostTag'
+    )
     objects = PostManager()
     plain:models.Manager['Post'] = models.Manager()
 
@@ -70,11 +75,11 @@ class Tag(BaseModel) :
     def __str__(self)->str :
         return self.caption.capitalize()
 
-class PostTag(BaseModel) :
-    tag = models.ForeignKey(Tag , on_delete=models.CASCADE)
-    post = models.ForeignKey(Post , on_delete=models.CASCADE)
+# class PostTag(BaseModel) :
+#     tag = models.ForeignKey(Tag , on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post , on_delete=models.CASCADE)
 
-    class Meta : # type: ignore
-        constraints = [
-            models.UniqueConstraint(fields=['tag' , 'post'] , name='unique_post_tag')
-        ]
+#     class Meta : # type: ignore
+#         constraints = [
+#             models.UniqueConstraint(fields=['tag' , 'post'] , name='unique_post_tag')
+#         ]
