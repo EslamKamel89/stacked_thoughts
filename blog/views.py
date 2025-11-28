d0f1cc2a56d79fec92d33b4b278351e54eba6d2d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-28 07:39:53 +0200

    refactor(blog): simplify latest posts logic using get_queryset

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 
 from django.db.models.manager import BaseManager
+from django.db.models.query import QuerySet
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
@@ -9,17 +10,12 @@ from django.views.generic.list import ListView
 from blog.models import Post
 
 
-def get_latest_posts(n:int = 1) ->BaseManager[Post] :
-    posts= Post.objects.prefetch_related('tags').order_by('-id')[:n]
-    return posts
-
 class StartingPageView(ListView):
     template_name = 'blog/index.html'
     model=Post
     context_object_name = 'posts'
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        # context =  super().get_context_data(**kwargs)
-        return {'posts' : get_latest_posts()}
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().prefetch_related('tags').order_by('-id')[:1]
 
 class BlogsView(ListView):
     template_name = 'blog/all-blogs.html'
