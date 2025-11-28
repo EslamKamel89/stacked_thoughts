5bb656b3a94121826150871dc859f31376ff04a1
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-28 07:33:35 +0200

    refactor(blog): convert function-based views to class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns : list[URLPattern] = [
-    path('' , views.starting_page , name='blog_home') ,
-    path('blogs/' , views.blogs , name="all_blogs") ,
-    path('blogs/<slug:slug>' , views.blog_detail , name='blog_detail')
+    path('' , views.StartingPageView.as_view() , name='blog_home') ,
+    path('blogs/' , views.BlogsView.as_view() , name="all_blogs") ,
+    path('blogs/<slug:slug>' , views.BlogDetailView.as_view() , name='blog_detail')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from typing import Dict
+from typing import Any, Dict
 
 from django.db.models.manager import BaseManager
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from blog.models import Post
 
@@ -11,26 +13,24 @@ def get_latest_posts(n:int = 1) ->BaseManager[Post] :
     posts= Post.objects.prefetch_related('tags').order_by('-id')[:n]
     return posts
 
-def find_post_by_slug(slug:str)->Post|None:
-    post = Post.objects.prefetch_related('tags').filter(slug=slug).first()
-    return post
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model=Post
+    context_object_name = 'posts'
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        # context =  super().get_context_data(**kwargs)
+        return {'posts' : get_latest_posts()}
 
-def starting_page(request:HttpRequest)->HttpResponse :
-    context:Dict[str, BaseManager[Post]] = {'posts' : get_latest_posts()}
-    return render(request , 'blog/index.html' , context)
+class BlogsView(ListView):
+    template_name = 'blog/all-blogs.html'
+    model = Post
+    context_object_name = 'posts'
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        return {'posts' : Post.objects.prefetch_related('tags')}
 
-def blogs(request:HttpRequest)->HttpResponse :
-    context:Dict[str, BaseManager[Post]] = {'posts' : Post.objects.prefetch_related('tags')}
-    return render(request, 'blog/all-blogs.html' , context)
-
-def blog_detail(request:HttpRequest , slug:str)->HttpResponse :
-    post:Post|None = find_post_by_slug(slug)
-    if post is None :
-        # return render(request , '404.html' )
-        raise Http404('Page not found')
-    context:Dict[str, Post] = {'post':post}
-    return render(request , 'blog/post-details.html' , context)
-
-
+class BlogDetailView(DetailView):
+    template_name = 'blog/post-details.html'
+    model=Post
+    context_object_name = 'post'
 
 
